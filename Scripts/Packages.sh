5341530cff4ff112661fcc1a9d5c478cd05e7ffc
sos801107 <108792640+sos801107@users.noreply.github.com>
2025-08-05 12:37:45 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -70,6 +70,10 @@ UPDATE_PACKAGE "qmodem" "FUjr/QModem" "main"
 UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-app-wolplus"
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
+rm -rf package/feeds/packages/sing-box
+UPDATE_PACKAGE "sing-box" "sos801107/packages" "main" "" "sing-box"
+
+
 #UPDATE_PACKAGE "luci-app-vlmcsd" "AutoCONFIG/luci-app-vlmcsd" "master"
 #UPDATE_PACKAGE "kenzok8" "kenzok8/openwrt-packages" "master" "" "luci-app-store"
 
